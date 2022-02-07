004f49a998d4a121e1588cf4df5c91d6958bbdc1
Jangjh93 <69702946+Jangjh93@users.noreply.github.com>
2022-02-07 17:54:39 +0900

    Update python_code.py

--- python_code.py
+++ python_code.py
@@ -1,3 +1,620 @@
+```python
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+```
+
+    C:\Users\Juhyeon\Anaconda3\lib\importlib\_bootstrap.py:219: RuntimeWarning: numpy.ufunc size changed, may indicate binary incompatibility. Expected 192 from C header, got 216 from PyObject
+      return f(*args, **kwds)
+    
+
+
+```python
+origin_dataset = pd.read_csv("C:/Users/Juhyeon/Documents/Juhyeon/breast_cancer/breast-cancer.csv")
+dataset = origin_dataset.iloc[:, 1:12]
+X = origin_dataset.iloc[:, 2:12].values
+y = origin_dataset.iloc[:, 1].values
+```
+
+
+```python
+origin_dataset.head()
+```
+
+
+
+
+<div>
+<style scoped>
+    .dataframe tbody tr th:only-of-type {
+        vertical-align: middle;
+    }
+
+    .dataframe tbody tr th {
+        vertical-align: top;
+    }
+
+    .dataframe thead th {
+        text-align: right;
+    }
+</style>
+<table border="1" class="dataframe">
+  <thead>
+    <tr style="text-align: right;">
+      <th></th>
+      <th>id</th>
+      <th>diagnosis</th>
+      <th>radius_mean</th>
+      <th>texture_mean</th>
+      <th>perimeter_mean</th>
+      <th>area_mean</th>
+      <th>smoothness_mean</th>
+      <th>compactness_mean</th>
+      <th>concavity_mean</th>
+      <th>concave points_mean</th>
+      <th>...</th>
+      <th>radius_worst</th>
+      <th>texture_worst</th>
+      <th>perimeter_worst</th>
+      <th>area_worst</th>
+      <th>smoothness_worst</th>
+      <th>compactness_worst</th>
+      <th>concavity_worst</th>
+      <th>concave points_worst</th>
+      <th>symmetry_worst</th>
+      <th>fractal_dimension_worst</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>0</td>
+      <td>842302</td>
+      <td>M</td>
+      <td>17.99</td>
+      <td>10.38</td>
+      <td>122.80</td>
+      <td>1001.0</td>
+      <td>0.11840</td>
+      <td>0.27760</td>
+      <td>0.3001</td>
+      <td>0.14710</td>
+      <td>...</td>
+      <td>25.38</td>
+      <td>17.33</td>
+      <td>184.60</td>
+      <td>2019.0</td>
+      <td>0.1622</td>
+      <td>0.6656</td>
+      <td>0.7119</td>
+      <td>0.2654</td>
+      <td>0.4601</td>
+      <td>0.11890</td>
+    </tr>
+    <tr>
+      <td>1</td>
+      <td>842517</td>
+      <td>M</td>
+      <td>20.57</td>
+      <td>17.77</td>
+      <td>132.90</td>
+      <td>1326.0</td>
+      <td>0.08474</td>
+      <td>0.07864</td>
+      <td>0.0869</td>
+      <td>0.07017</td>
+      <td>...</td>
+      <td>24.99</td>
+      <td>23.41</td>
+      <td>158.80</td>
+      <td>1956.0</td>
+      <td>0.1238</td>
+      <td>0.1866</td>
+      <td>0.2416</td>
+      <td>0.1860</td>
+      <td>0.2750</td>
+      <td>0.08902</td>
+    </tr>
+    <tr>
+      <td>2</td>
+      <td>84300903</td>
+      <td>M</td>
+      <td>19.69</td>
+      <td>21.25</td>
+      <td>130.00</td>
+      <td>1203.0</td>
+      <td>0.10960</td>
+      <td>0.15990</td>
+      <td>0.1974</td>
+      <td>0.12790</td>
+      <td>...</td>
+      <td>23.57</td>
+      <td>25.53</td>
+      <td>152.50</td>
+      <td>1709.0</td>
+      <td>0.1444</td>
+      <td>0.4245</td>
+      <td>0.4504</td>
+      <td>0.2430</td>
+      <td>0.3613</td>
+      <td>0.08758</td>
+    </tr>
+    <tr>
+      <td>3</td>
+      <td>84348301</td>
+      <td>M</td>
+      <td>11.42</td>
+      <td>20.38</td>
+      <td>77.58</td>
+      <td>386.1</td>
+      <td>0.14250</td>
+      <td>0.28390</td>
+      <td>0.2414</td>
+      <td>0.10520</td>
+      <td>...</td>
+      <td>14.91</td>
+      <td>26.50</td>
+      <td>98.87</td>
+      <td>567.7</td>
+      <td>0.2098</td>
+      <td>0.8663</td>
+      <td>0.6869</td>
+      <td>0.2575</td>
+      <td>0.6638</td>
+      <td>0.17300</td>
+    </tr>
+    <tr>
+      <td>4</td>
+      <td>84358402</td>
+      <td>M</td>
+      <td>20.29</td>
+      <td>14.34</td>
+      <td>135.10</td>
+      <td>1297.0</td>
+      <td>0.10030</td>
+      <td>0.13280</td>
+      <td>0.1980</td>
+      <td>0.10430</td>
+      <td>...</td>
+      <td>22.54</td>
+      <td>16.67</td>
+      <td>152.20</td>
+      <td>1575.0</td>
+      <td>0.1374</td>
+      <td>0.2050</td>
+      <td>0.4000</td>
+      <td>0.1625</td>
+      <td>0.2364</td>
+      <td>0.07678</td>
+    </tr>
+  </tbody>
+</table>
+<p>5 rows × 32 columns</p>
+</div>
+
+
+
+
+```python
+dataset.head()
+```
+
+
+
+
+<div>
+<style scoped>
+    .dataframe tbody tr th:only-of-type {
+        vertical-align: middle;
+    }
+
+    .dataframe tbody tr th {
+        vertical-align: top;
+    }
+
+    .dataframe thead th {
+        text-align: right;
+    }
+</style>
+<table border="1" class="dataframe">
+  <thead>
+    <tr style="text-align: right;">
+      <th></th>
+      <th>diagnosis</th>
+      <th>radius_mean</th>
+      <th>texture_mean</th>
+      <th>perimeter_mean</th>
+      <th>area_mean</th>
+      <th>smoothness_mean</th>
+      <th>compactness_mean</th>
+      <th>concavity_mean</th>
+      <th>concave points_mean</th>
+      <th>symmetry_mean</th>
+      <th>fractal_dimension_mean</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>0</td>
+      <td>M</td>
+      <td>17.99</td>
+      <td>10.38</td>
+      <td>122.80</td>
+      <td>1001.0</td>
+      <td>0.11840</td>
+      <td>0.27760</td>
+      <td>0.3001</td>
+      <td>0.14710</td>
+      <td>0.2419</td>
+      <td>0.07871</td>
+    </tr>
+    <tr>
+      <td>1</td>
+      <td>M</td>
+      <td>20.57</td>
+      <td>17.77</td>
+      <td>132.90</td>
+      <td>1326.0</td>
+      <td>0.08474</td>
+      <td>0.07864</td>
+      <td>0.0869</td>
+      <td>0.07017</td>
+      <td>0.1812</td>
+      <td>0.05667</td>
+    </tr>
+    <tr>
+      <td>2</td>
+      <td>M</td>
+      <td>19.69</td>
+      <td>21.25</td>
+      <td>130.00</td>
+      <td>1203.0</td>
+      <td>0.10960</td>
+      <td>0.15990</td>
+      <td>0.1974</td>
+      <td>0.12790</td>
+      <td>0.2069</td>
+      <td>0.05999</td>
+    </tr>
+    <tr>
+      <td>3</td>
+      <td>M</td>
+      <td>11.42</td>
+      <td>20.38</td>
+      <td>77.58</td>
+      <td>386.1</td>
+      <td>0.14250</td>
+      <td>0.28390</td>
+      <td>0.2414</td>
+      <td>0.10520</td>
+      <td>0.2597</td>
+      <td>0.09744</td>
+    </tr>
+    <tr>
+      <td>4</td>
+      <td>M</td>
+      <td>20.29</td>
+      <td>14.34</td>
+      <td>135.10</td>
+      <td>1297.0</td>
+      <td>0.10030</td>
+      <td>0.13280</td>
+      <td>0.1980</td>
+      <td>0.10430</td>
+      <td>0.1809</td>
+      <td>0.05883</td>
+    </tr>
+  </tbody>
+</table>
+</div>
+
+
+
+
+```python
+dataset.shape
+```
+
+
+
+
+    (569, 11)
+
+
+
+
+```python
+pd.DataFrame(dataset).dtypes
+```
+
+
+
+
+    diagnosis                  object
+    radius_mean               float64
+    texture_mean              float64
+    perimeter_mean            float64
+    area_mean                 float64
+    smoothness_mean           float64
+    compactness_mean          float64
+    concavity_mean            float64
+    concave points_mean       float64
+    symmetry_mean             float64
+    fractal_dimension_mean    float64
+    dtype: object
+
+
+
+
+```python
+pd.DataFrame(dataset)['diagnosis'].unique()
+```
+
+
+
+
+    array(['M', 'B'], dtype=object)
+
+
+
+
+```python
+# diagnosis rate
+pos = (pd.DataFrame(dataset)['diagnosis']=="M").sum()
+neg = (pd.DataFrame(dataset)['diagnosis']=="B").sum()
+np.array([['pos', 'neg'],[round(pos/dataset.shape[0],2),round(neg/dataset.shape[0],2)]])
+```
+
+
+
+
+    array([['pos', 'neg'],
+           ['0.37', '0.63']], dtype='<U4')
+
+
+
+
+```python
+# missing value check -> no imputation needed
+pd.DataFrame(dataset).isnull().sum().sort_values(ascending = True)
+```
+
+
+
+
+    diagnosis                 0
+    radius_mean               0
+    texture_mean              0
+    perimeter_mean            0
+    area_mean                 0
+    smoothness_mean           0
+    compactness_mean          0
+    concavity_mean            0
+    concave points_mean       0
+    symmetry_mean             0
+    fractal_dimension_mean    0
+    dtype: int64
+
+
+
+
+```python
+# Encoding the Dependent Variable (one hot encoding is not needed for independent variables here)
+from sklearn.preprocessing import LabelEncoder
+le = LabelEncoder()
+y = le.fit_transform(y)
+print(y)
+```
+
+    [1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 0 0 0 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+     0 1 1 1 1 1 1 1 1 0 1 0 0 0 0 0 1 1 0 1 1 0 0 0 0 1 0 1 1 0 0 0 0 1 0 1 1
+     0 1 0 1 1 0 0 0 1 1 0 1 1 1 0 0 0 1 0 0 1 1 0 0 0 1 1 0 0 0 0 1 0 0 1 0 0
+     0 0 0 0 0 0 1 1 1 0 1 1 0 0 0 1 1 0 1 0 1 1 0 1 1 0 0 1 0 0 1 0 0 0 0 1 0
+     0 0 0 0 0 0 0 0 1 0 0 0 0 1 1 0 1 0 0 1 1 0 0 1 1 0 0 0 0 1 0 0 1 1 1 0 1
+     0 1 0 0 0 1 0 0 1 1 0 1 1 1 1 0 1 1 1 0 1 0 1 0 0 1 0 1 1 1 1 0 0 1 1 0 0
+     0 1 0 0 0 0 0 1 1 0 0 1 0 0 1 1 0 1 0 0 0 0 1 0 0 0 0 0 1 0 1 1 1 1 1 1 1
+     1 1 1 1 1 1 1 0 0 0 0 0 0 1 0 1 0 0 1 0 0 1 0 1 1 0 0 0 0 0 0 0 0 0 0 0 0
+     0 1 0 0 1 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 1 0 1 0 0 0 0 1 1 1 0 0
+     0 0 1 0 1 0 1 0 0 0 1 0 0 0 0 0 0 0 1 1 1 0 0 0 0 0 0 0 0 0 0 0 1 1 0 1 1
+     1 0 1 1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 1 0 0 1 1 0 0 0 0 0 0 1 0 0 0 0 0 0
+     0 1 0 0 0 0 0 1 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 1 0 1 1 0 1 0 0 0 0 0 1 0 0
+     1 0 1 0 0 1 0 1 0 0 0 0 0 0 0 0 1 1 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 1 0
+     0 0 0 0 0 0 1 0 1 0 0 1 0 0 0 0 0 1 1 0 1 0 1 0 0 0 0 0 1 0 0 1 0 1 0 1 1
+     0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 1 0 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+     0 0 0 0 0 0 0 1 1 1 1 1 1 0]
+    
+
+
+```python
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 1)
+```
+
+    C:\Users\Juhyeon\Anaconda3\lib\importlib\_bootstrap.py:219: RuntimeWarning: numpy.ufunc size changed, may indicate binary incompatibility. Expected 192 from C header, got 216 from PyObject
+      return f(*args, **kwds)
+    
+
+
+```python
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+```
+
+
+```python
+# Logistic Regression
+np.warnings.filterwarnings("ignore")
+from sklearn.linear_model import LogisticRegression
+m1 = LogisticRegression(random_state=0)
+m1.fit(X_train,y_train)
+```
+
+
+
+
+    LogisticRegression(C=1.0, class_weight=None, dual=False, fit_intercept=True,
+                       intercept_scaling=1, l1_ratio=None, max_iter=100,
+                       multi_class='warn', n_jobs=None, penalty='l2',
+                       random_state=0, solver='warn', tol=0.0001, verbose=0,
+                       warm_start=False)
+
+
+
+
+```python
+# Accuracy, Confusion Matrix
+from sklearn.metrics import confusion_matrix, accuracy_score
+y_pred = m1.predict(sc.transform(X_test))
+cm = confusion_matrix(y_test,y_pred)
+acc = accuracy_score(y_test,y_pred)
+print(cm)
+print("Accuracy: %.3f" % acc)
+```
+
+    [[90 18]
+     [16 47]]
+    Accuracy: 0.801
+    
+
+
+```python
+# SVM
+from sklearn.svm import SVC
+m2 = SVC(random_state=0, kernel='rbf')
+m2.fit(X_train, y_train)
+```
+
+
+
+
+    SVC(C=1.0, cache_size=200, class_weight=None, coef0=0.0,
+        decision_function_shape='ovr', degree=3, gamma='auto_deprecated',
+        kernel='rbf', max_iter=-1, probability=False, random_state=0,
+        shrinking=True, tol=0.001, verbose=False)
+
+
+
+
+```python
+# Accuracy, Confusion Matrix
+from sklearn.metrics import confusion_matrix, accuracy_score
+y_pred = m2.predict(sc.transform(X_test))
+cm = confusion_matrix(y_test,y_pred)
+acc = accuracy_score(y_test,y_pred)
+print(cm)
+print("Accuracy: %.3f" % acc)
+```
+
+    [[108   0]
+     [ 63   0]]
+    Accuracy: 0.632
+    
+
+
+```python
+# Naive bayes
+from sklearn.neighbors import KNeighborsClassifier
+m3 = KNeighborsClassifier()
+m3.fit(X_train,y_train)
+```
+
+
+
+
+    KNeighborsClassifier(algorithm='auto', leaf_size=30, metric='minkowski',
+                         metric_params=None, n_jobs=None, n_neighbors=5, p=2,
+                         weights='uniform')
+
+
+
+
+```python
+# Accuracy, Confusion Matrix
+from sklearn.metrics import confusion_matrix, accuracy_score
+y_pred = m3.predict(sc.transform(X_test))
+cm = confusion_matrix(y_test,y_pred)
+acc = accuracy_score(y_test,y_pred)
+print(cm)
+print("Accuracy: %.3f" % acc)
+```
+
+    [[106   2]
+     [ 38  25]]
+    Accuracy: 0.766
+    
+
+
+```python
+# RF
+from sklearn.ensemble import RandomForestClassifier
+m4 = RandomForestClassifier(n_estimators=10,random_state=0)
+m4.fit(X_train, y_train)
+```
+
+
+
+
+    RandomForestClassifier(bootstrap=True, class_weight=None, criterion='gini',
+                           max_depth=None, max_features='auto', max_leaf_nodes=None,
+                           min_impurity_decrease=0.0, min_impurity_split=None,
+                           min_samples_leaf=1, min_samples_split=2,
+                           min_weight_fraction_leaf=0.0, n_estimators=10,
+                           n_jobs=None, oob_score=False, random_state=0, verbose=0,
+                           warm_start=False)
+
+
+
+
+```python
+# Accuracy, Confusion Matrix
+from sklearn.metrics import confusion_matrix, accuracy_score
+y_pred = m4.predict(sc.transform(X_test))
+cm = confusion_matrix(y_test,y_pred)
+acc = accuracy_score(y_test,y_pred)
+print(cm)
+print("Accuracy: %.3f" % acc)
+```
+
+    [[108   0]
+     [ 63   0]]
+    Accuracy: 0.632
+    
+
+
+```python
+# Decision Tree
+from sklearn.tree import DecisionTreeClassifier
+m5 = DecisionTreeClassifier(random_state=0, criterion='entropy')
+m5.fit(X_train, y_train)
+```
+
+
+
+
+    DecisionTreeClassifier(class_weight=None, criterion='entropy', max_depth=None,
+                           max_features=None, max_leaf_nodes=None,
+                           min_impurity_decrease=0.0, min_impurity_split=None,
+                           min_samples_leaf=1, min_samples_split=2,
+                           min_weight_fraction_leaf=0.0, presort=False,
+                           random_state=0, splitter='best')
+
+
+
+
+```python
+# Accuracy, Confusion Matrix
+from sklearn.metrics import confusion_matrix, accuracy_score
+y_pred = m5.predict(sc.transform(X_test))
+cm = confusion_matrix(y_test,y_pred)
+acc = accuracy_score(y_test,y_pred)
+print(cm)
+print("Accuracy: %.3f" % acc)
+```
+
+    [[108   0]
+     [ 63   0]]
+    Accuracy: 0.632
+    
